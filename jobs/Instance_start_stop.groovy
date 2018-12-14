fbf4cabd051212492da4ca83750e921ced7980a9
venu <venualonea1@gmail.com>
2018-12-15 00:37:48 +0530

    Adding the artifacts

--- jobs/Instance_start_stop.groovy
+++ jobs/Instance_start_stop.groovy
@@ -1,12 +1,33 @@
 action="${env.Action}"
 
+
+
+
 pipeline{
-	steps{
-		script{
+		steps{
+			script{
 	
-	instanceProp = readProperties file : "./properties/instance.properties"
-		echo "where is my name"
+		instanceProp = readProperties file : "./properties/instance.properties"
+				echo "where is my name"
 	
+	switch(action.toUpperCase()){
+	case "JVM-1"
+	echo "Starting the 1st JVM"
+	startApp = "/opt/TOMCAT_INSTANCE-1/bin/startup.sh"
+	startresult = sh script: "${startApp}" returnStatus:true
+	if(startresult == 0){
+	echo"The JVM is started"
+	
+	}else{
+		echo "The JVMM is not started"
+	}
+	
+	
+	
+	break;
+	
+	
+	}
 	
 		}
 	}
