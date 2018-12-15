41b701779d97206f0ab39c9b0cb6d2f7aacaf0cd
venu <venualonea1@gmail.com>
2018-12-16 00:16:38 +0530

    Adding the JVM instance 2

--- jobs/Instance_start_stop.groovy
+++ jobs/Instance_start_stop.groovy
@@ -29,11 +29,24 @@ pipeline{
 				}else{
 					echo "The JVMM is not started"
 							}
+				break;
+				case "JVM-2"
+				echo "Starting the 1st JVM"
+				StatCommand = "ps -aef|grep -i TOMCAT_INSTANCE-2 |grep -v grep"
+				Process_Command = "${StatCommand}"
+				
+				//startApp = "/opt/TOMCAT_INSTANCE-1/bin/startup.sh"
+				startresult = sh script: "${Process_Command}", returnStatus:true
+				if(startresult == 0){
+				echo"The TOMCAT_INSTANCE-2 is started"
 	
+				}else{
+					echo "The TOMCAT_INSTANCE-2 is not started"
+							}
 	
 	
 				break;
-	
+\ 
 	
 					}
 	
