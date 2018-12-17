f3ffd83cce708fab895eb60704eb2f58ee0d7e6b
venu <venualonea1@gmail.com>
2018-12-17 23:05:25 +0530

    Addiing:

---
+++ jobs/Node_App.groovy
@@ -0,0 +1,31 @@
+action="${env.Action}"
+
+
+pipeline{
+	agent{
+		node{
+			label 'master'
+		}
+	}
+	stages{
+		stage('PM2 Status Check'){
+		steps{
+			script{
+			Check_Command=" pm2 status"
+				CheckStatus= sh script: "${Check_Command}" ,returnStatus: true
+				
+				if(CheckStatus==0){
+				echo "showing the PM2 Status"
+				}else{
+				"Nothing is defined"
+				}
+				
+				
+			
+						}
+	
+				}
+			}
+		}
+	}
+} 
