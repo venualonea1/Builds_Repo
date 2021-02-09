24892a006807f5b67a79230b2f8e9c194c9d06c4
Venu Bharadwaj <venualonea1@gmail.com>
2021-02-09 17:57:52 +0530

    Create JenkinsFile.groovy

---
+++ JenkinsFile.groovy
@@ -0,0 +1,37 @@
+import hudson.Filepath
+import jenkins.model.Jenkins
+pipeline{
+agent{
+	node{
+		label 'shivani'
+		
+	}
+}
+
+stages {
+  stage('Stage1: Go to Root'){
+  steps{
+  script{
+		try{
+			timeout(time:300, unit:'SECONDS'){//change the conventional times 
+			userInput = input(
+			id: 'Proceed1' ,message: 'Go ahead with parameters', parameters:[
+			[$class: 'FileParameterDefination',name: 'FilePath', description:"This is the fileupload test"]
+			]
+			)
+			}
+			
+		}catch(err){
+			
+			if("SYSTEM" == user.toString()){
+				didTimeout = true
+				echo "InputTime Out"
+			}
+		}
+  
+				}
+			}
+		}
+	}
+
+}
