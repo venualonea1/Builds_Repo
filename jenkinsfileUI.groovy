ed5b47fa825531e8f0a5725d1ad463a42d053e5f
Venu Bharadwaj <venualonea1@gmail.com>
2021-10-10 07:56:54 +0530

    Update jenkinsfileUI.groovy

--- jenkinsfileUI.groovy
+++ jenkinsfileUI.groovy
@@ -6,12 +6,11 @@ stages {
   steps{
   script{
         echo "Checking the AWS Cli Installation"
-	  
-          sh 'curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64-2.0.30.zip" -o "awscliv2.zip"'
-sh 'sudo su; unzip -o awscliv2.zip'
-sh 'sudo ./aws/install'
-
-
+	 	sh  'aws -version'
+	  	withCredentials([usernamePassword(credentialsId: 'AWSS3', passwordVariable: 'S3Uname', usernameVariable: 'S3Uname')]) {
+			sh 'aws s3 ls'
+    				
+}
 
 				}
 			}
