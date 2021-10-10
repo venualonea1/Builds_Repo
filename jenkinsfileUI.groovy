c2866d329d44df818f8e5b5781fc426a4a842757
Venu Bharadwaj <venualonea1@gmail.com>
2021-10-10 07:46:52 +0530

    Update jenkinsfileUI.groovy

--- jenkinsfileUI.groovy
+++ jenkinsfileUI.groovy
@@ -8,7 +8,7 @@ stages {
         echo "Checking the AWS Cli Installation"
 	  
           sh 'curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64-2.0.30.zip" -o "awscliv2.zip"'
-sh 'unzip awscliv2.zip'
+sh 'sudo unzip -o awscliv2.zip'
 sh 'sudo ./aws/install'
 
 
