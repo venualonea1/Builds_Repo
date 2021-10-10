fb08bc22d2cd3e263b1e4525a5093f46a5bbb8c7
Venu Bharadwaj <venualonea1@gmail.com>
2021-10-10 08:09:39 +0530

    Update jenkinsfileUI.groovy

--- jenkinsfileUI.groovy
+++ jenkinsfileUI.groovy
@@ -2,7 +2,7 @@ pipeline{
 agent any
  environment {
         AWS_ACCESS_KEY_ID     = credentials('aws-jenkins-user-id')
-        AWS_SECRET_ACCESS_KEY = credentials('ws-jenkins-user-name')
+        AWS_SECRET_ACCESS_KEY = credentials('aws-jenkins-user-name')
     }
 	
 stages {
