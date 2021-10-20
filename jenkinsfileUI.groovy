0a045684d3cc31272b911673d37e60ede2006839
Venu Bharadwaj <venualonea1@gmail.com>
2021-10-20 23:23:35 +0530

    Update jenkinsfileUI.groovy

--- jenkinsfileUI.groovy
+++ jenkinsfileUI.groovy
@@ -16,7 +16,7 @@ agent any
 	
 	
 	parameters{
-	choice(name: 'PERSON', defaultValue: 'Mr Jenkins', description: 'Who should I say hello to?)
+	string(name: 'PERSON', defaultValue: 'Mr Jenkins', description: 'Who should I say hello to?)
 	
 	}
 	
